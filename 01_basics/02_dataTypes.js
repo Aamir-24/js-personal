a521d2c555703c604ab0eef02343460d2f7c2342
Aamir Mateen <aamirmateen34@gmail.com>
2024-04-06 00:53:22 +0000

    DataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let age = 22
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// BigInt
+// String
+// Boolean => true/false
+// null => standalone value 
+// undefined
+// Symbol => unique
+
+// obect
+
+console.log(age)
+console.log(isLoggedIn)
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
