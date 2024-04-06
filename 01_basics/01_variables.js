660f0d42bdaad48f32a789f687c2173773c0931b
Aamir Mateen <aamirmateen34@gmail.com>
2024-04-06 00:44:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 55550;
+
+let accountEmail = "aamir@gmail.com"
+var accountPassword = "1234"
+accountCity = "Patna"
+
+// accountId = 5
+// // console.log(accountId);  we cannot cange constant value
+// accountEmail = "aaa"
+// accountPassword = "111"  -- We can reassigne leat and var value
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
